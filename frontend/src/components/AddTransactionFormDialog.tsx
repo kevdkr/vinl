50cdf7bffd252c9f272f932ffd88255002702b56
kkrohn18 <48852657+kkrohn18@users.noreply.github.com>
2023-02-19 13:43:03 -0600

    rename FormDialog

--- frontend/src/components/AddTransactionFormDialog.tsx
+++ frontend/src/components/AddTransactionFormDialog.tsx
@@ -1,20 +1,19 @@
-import * as React from 'react';
+import { useForm, useFieldArray, useWatch, Control } from "react-hook-form";
+
 import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import InputAdornment from '@mui/material/InputAdornment';
 
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
 import OutlinedInput from '@mui/material/OutlinedInput';
-import Input from '@mui/material/Input';
-import FilledInput from '@mui/material/FilledInput';
+import Transaction from '../models/Transaction';
+import React, { useState, useEffect } from 'react';
 
 const fabStyle = {
   position: 'fixed',
@@ -22,82 +21,66 @@ const fabStyle = {
   right: 30,
 };
 
-interface Transaction {
+type FormValues = {
   date: string;
   payee: string;
   payeeComment: string;
   comment: string;
-  accounts: [
-      {
-          name: string;
-          amount: string;
-          comment: string;
-          is_comment: boolean;
-      }
-  ]
+  accounts: {
+    name: string;
+    amount: string;
+    comment: string;
+    //is_comment: boolean;
+  }[];
   isComment: boolean;
+};
+
+let today: Date = new Date();
+let dd: string = String(today.getDate()).padStart(2, '0');
+let mm: string = String(today.getMonth() + 1).padStart(2, '0');
+let yyyy: string = String(today.getFullYear());
+
+let todayDate: string = yyyy + "/" + mm + "/" + dd;
+
+type Props = {
+  saveTransaction: (formData: Transaction | any) => void
 }
 
-export default function FormDialog() {
+const FormDialog: React.FC<Props> = ({ saveTransaction }) => {
   const [open, setOpen] = React.useState(false);
-
+  //const [transactions, setTransactions] = React.useState(transactionsPrev);
   const handleClickOpen = () => {
     setOpen(true);
   };
-
   const handleClose = () => {
     setOpen(false);
   };
 
-
-  // const [values, setValues] = React.useState({
-  //   date: '',
-  //   payee: '',
-  //   payeeComment: '',
-  //   comment: '',
-  //   accounts: [
-  //       {
-  //           name: '',
-  //           amount: '',
-  //           comment: '',
-  //           is_comment: false,
-  //       },
-  //   ],
-  //   isComment: false,
-  // });
-
-  const [transactionValues, setTransactionValues] = React.useState({
-    date: '',
-    payee: '',
-    payeeComment: '',
-    comment: '',
-    isComment: false,
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormValues>({
+    defaultValues: {
+      //accounts: [{ name: "test", amount: "20.00", comment: "test" }]
+    },
+    mode: "onBlur"
+  });
+  const { fields, append, remove } = useFieldArray({
+    name: "accounts",
+    control
   });
 
-  const [accountsValues, setAccountsValues] = React.useState({
-    accounts: [
-      {
-        name: '',
-        amount: '',
-        comment: '',
-        is_comment: false,
-      },
-    ],
-  });
-
-  // const handleTransactionValuesChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   setTransactionValues({...transactionValues,[event.target.name] : event.target.value});
-  // }
-
-  //const handleTransactionValuesChange = (e: any) => setTransactionValues(e.target.value);
-  //const handleSubmit = () => {
-  //  console.log(transactionValues)
- // }
-
-  const handleTransactionValuesChange = (event: any) => {
-    const date = event.target.date;
-    //setTransactionValues(values => ({...transactionValues, [name]: value}))
+  const onSubmit = (data: FormValues) => {
+    // fetch('transactions', {
+    //   method: 'POST',
+    //   body: JSON.stringify(data)
+    // })
+    saveTransaction(data)
+    handleClose();
   }
+
   return (
     <div>
       <Fab sx={fabStyle} color="primary" aria-label="add" onClick={handleClickOpen}>
@@ -106,58 +89,103 @@ export default function FormDialog() {
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Add Transaction</DialogTitle>
         <DialogContent>
-          //<FormControl fullWidth sx={{ m: 1 }}>
-            <InputLabel htmlFor="outlined-adornment-amount">Date</InputLabel>
-            <OutlinedInput
-                id="outlined-adornment-amount"
-                value={transactionValues.date}
-                onChange={handleTransactionValuesChange}
-                label="Date"
-            />
-          //</FormControl>
 
-          //<FormControl fullWidth sx={{ m: 1 }}>
-            <InputLabel htmlFor="outlined-adornment-amount">Payee</InputLabel>
-            <OutlinedInput
-                id="outlined-adornment-amount"
-                value={transactionValues.payee}
-                onChange={handleTransactionValuesChange}
-                label="Payee"
-            />
-          //</FormControl>
-          //<FormControl fullWidth sx={{ m: 1 }}>
-            <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
-            <OutlinedInput
-                id="outlined-adornment-amount"
-                //value={values.amount}
-                //onChange={handleChange('amount')}
-                startAdornment={<InputAdornment position="start">$</InputAdornment>}
-                label="Amount"
+      <form onSubmit={handleSubmit(onSubmit)}>
+
+        <div>
+        <FormControl fullWidth sx={{ m: 1 }}>
+          <InputLabel htmlFor="outlined-adornment-date">Date</InputLabel>
+          <OutlinedInput
+            defaultValue={todayDate}
+            {...register("date")}
+            id="outlined-adornment-date"
+            label="Date"
           />
-        //</FormControl>
-        //<FormControl fullWidth sx={{ m: 1 }} variant="filled">
-          <InputLabel htmlFor="filled-adornment-amount">Amount</InputLabel>
-          <FilledInput
-            id="filled-adornment-amount"
-            //value={values.amount}
-            //onChange={handleChange('amount')}
-            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+        </FormControl>
+        <FormControl fullWidth sx={{ m: 1 }}>
+          <InputLabel htmlFor="outlined-adornment-payee">Payee</InputLabel>
+          <OutlinedInput
+            {...register("payee")}
+            id="outlined-adornment-payee"
+            label="Payee"
           />
-        //</FormControl>
-        //<FormControl fullWidth sx={{ m: 1 }} variant="standard">
-          <InputLabel htmlFor="standard-adornment-amount">Amount</InputLabel>
-          <Input
-            id="standard-adornment-amount"
-            //value={values.amount}
-            //onChange={handleChange('amount')}
-            startAdornment={<InputAdornment position="start">$</InputAdornment>}
-          />
-        //</FormControl>
-        </DialogContent>
-        <DialogActions>
-          //<Button onClick={handleClose}>Cancel</Button>
-        </DialogActions>
+        </FormControl>
+        </div>
+
+        {fields.map((field, index) => {
+          return (
+            <div key={field.id}>
+              Account
+              <section className={"section"} key={field.id}>
+                <FormControl fullWidth sx={{ m: 1 }}>
+                  <InputLabel htmlFor="outlined-adornment-name">Name</InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-name"
+                    label="Name"
+                    {...register(`accounts.${index}.name` as const, {
+                      required: true
+                    })}
+                    className={errors?.accounts?.[index]?.name ? "error" : ""}
+                    defaultValue={field.name}
+                  />
+                </FormControl>
+                <FormControl fullWidth sx={{ m: 1 }}>
+                  <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-amount"
+                    startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                    label="Amount"
+                    type="string"
+                    {...register(`accounts.${index}.amount` as const, {
+                      valueAsNumber: false,
+                      required: false
+                    })}
+                    className={errors?.accounts?.[index]?.amount ? "error" : ""}
+                    defaultValue={field.amount}
+                  />
+               </FormControl>
+              <FormControl fullWidth sx={{ m: 1 }}>
+                <InputLabel htmlFor="outlined-adornment-comment">Comment</InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-comment"
+                  label="Comment"
+                  type="string"
+                  {...register(`accounts.${index}.comment` as const, {
+                    valueAsNumber: false,
+                    required: false
+                  })}
+                  className={errors?.accounts?.[index]?.comment ? "error" : ""}
+                  defaultValue={field.comment}
+                />
+              </FormControl>
+                <Button onClick={() => remove(index)}>
+                  Delete
+                </Button>
+              </section>
+            </div>
+          );
+        })}
+
+        <Button
+          onClick={() =>
+            append({
+              name: "",
+              amount: "",
+              comment: "",
+            })
+          }
+        >
+          Add Account
+        </Button>
+      <Button onClick={handleSubmit(onSubmit)}>Submit</Button>
+      </form>
+      </DialogContent>
+      <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+      </DialogActions>
       </Dialog>
     </div>
   );
 }
+
+export default FormDialog
