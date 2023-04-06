940e8ebc8550d3e20c3f2d64089b6551da77a885
kkrohn18 <48852657+kkrohn18@users.noreply.github.com>
2023-04-05 19:07:17 -0500

    add AccountList component

---
+++ frontend/src/components/AccountList.tsx
@@ -0,0 +1,55 @@
+import Account from '../models/Account'
+import AccountItem from './AccountItem'
+import React, { useState, useEffect } from 'react';
+
+import List from '@mui/material/List';
+import { getAccounts, createAccount, deleteAccount, AccountFormValues } from '../services/Accounts'
+import FormAccountDialog from "./AddAccountFormDialog"
+
+const AccountList: React.FC = () => {
+
+    const [accounts, setAccounts] = useState<Account[]>([])
+
+    useEffect(() => {
+        getAccounts().then((response) => {
+            setAccounts(response);
+        })
+    }, [])
+
+    const handleDeleteAccount = (id: string): void => {
+        deleteAccount(id)
+            .then((response) => {
+                getAccounts().then((response) => {
+                    setAccounts(response);
+                })
+                return response;
+            })
+            .catch((err) => console.log(err))
+    }
+
+    const handleSaveAccount = (formData: AccountFormValues): void => {
+        createAccount(formData)
+            .then((response) => {
+                getAccounts().then((response) => {
+                    setAccounts(response);
+                })
+                return response;
+            })
+            .catch((err) => console.log(err))
+    }
+
+    return (
+        <div>
+            <List sx={{ height: '87%', width: '100%', position: 'fixed', bgcolor: 'background.paper', overflow: 'auto' }}>
+                {accounts.map(account =>
+                    <AccountItem
+                        key={account.id}
+                        account={account}
+                        deleteAccount={handleDeleteAccount}/>)}
+            </List>
+            <FormAccountDialog saveAccount={handleSaveAccount}/>
+        </div>
+    )
+};
+
+export default AccountList;
