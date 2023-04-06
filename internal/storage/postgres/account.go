7333d420113f2f4ae0b1106b0d57028d79ef9179
kkrohn18 <48852657+kkrohn18@users.noreply.github.com>
2023-04-05 19:29:09 -0500

    improve error handling in postgres account storage

--- internal/storage/postgres/account.go
+++ internal/storage/postgres/account.go
@@ -50,7 +50,6 @@ func (storage *PostgresAccountStorage) GetAccounts() (*[]models.Account, error) 
 		}
 		accounts = append(accounts, a)
 	}
-	//TODO handle if accounts has 0 rows
 	if accounts == nil {
 		return &[]models.Account{}, nil
 	}
@@ -65,7 +64,7 @@ func (storage *PostgresAccountStorage) GetAccountById(id string) (*models.Accoun
 	var name string
 	err := accountRow.Scan(&id, &name)
 	if err != nil {
-		return nil, fmt.Errorf("Error scanning account row onto vars: ", err)
+		return nil, fmt.Errorf("Error scanning account row onto vars: %q", err)
 	}
 	a = &models.Account{
 		Id: accountid,
@@ -78,16 +77,16 @@ func (storage *PostgresAccountStorage) DeleteAccountById(id string) error {
 	accountDeleteQuery := "DELETE FROM accounts WHERE id = $1" //TODO what is account is referenced by postings
 	res, err := storage.db.Exec(accountDeleteQuery, id)
 	if err != nil {
-		return fmt.Errorf("Error deleting account from database: ", err)
+		return fmt.Errorf("Error deleting account with id %q from accounts table: %q", id, err)
 	}
 	count, err := res.RowsAffected()
 	if err != nil {
-		return fmt.Errorf("Error getting count deleted from database: ", err)
+		return fmt.Errorf("Error getting count deleted from accounts table: %q", err)
 	}
 	if count != 1 {
-		log.Printf("Error: 1 row was supposed to be deleted, but %d were deleted", count)
+		return fmt.Errorf("Error: 1 row was supposed to be deleted from the accounts table (account with id %q), but %d rows were deleted", id, count)
 	} else {
-		log.Printf("Deleted %d accounts", count)
+		log.Printf("Deleted account with id %q from accounts table", id)
 	}
 	return nil
 }
