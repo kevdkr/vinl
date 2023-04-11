60c98b3f2c7d09d1ea92863a848f3cc60b328467
kkrohn18 <48852657+kkrohn18@users.noreply.github.com>
2023-04-10 22:22:06 -0500

    add setup script for local development

--- localsetup.sh
+++ localsetup.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+export DB_HOST=localhost
+export DB_PORT=5432
+export DB_USERNAME=vinl
+export DB_PASSWORD=test
+export DB_NAME=vinl
+export DB_SSLMODE=disable
+
 systemctl start docker;
 sleep 5;
 docker stop postgres && docker rm postgres
 docker run --name postgres -e POSTGRES_USERNAME=postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d postgres
 
 sleep 5;
-psql -U postgres -h localhost -f ./init.sql
+psql -U postgres -h localhost -f ../../init.sql
+
 
 go run .
