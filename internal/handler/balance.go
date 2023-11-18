537a11e0d99c6e317ea781255cd17cf13f2e620c
kkrohn18 <48852657+kkrohn18@users.noreply.github.com>
2023-11-18 15:22:43 -0600

    add handler for getting balance of accounts

--- internal/handler/balance.go
+++ internal/handler/balance.go
@@ -37,3 +37,21 @@ func (h *BalanceHandler) HandleGetBalanceOfAccount() http.HandlerFunc {
 		json.NewEncoder(w).Encode(balance)
 	}
 }
+
+func (h *BalanceHandler) HandleGetBalanceOfAccounts() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			log.Printf("405 Method not allowed")
+			return
+		}
+
+		balances, err := h.service.GetTotalDollarsOfAccounts()
+		if err != nil {
+			log.Printf("%v", fmt.Errorf("Error gtting balances of all accounts: %q", err))
+			return
+		}
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(balances)
+	}
+}
